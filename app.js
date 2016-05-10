047d924f800ed87dea4efab543274a4bf0106bdf
Jed Dolbeer <jdolbeer@gmail.com>
2016-05-10 15:48:30 -0700

    Finished JS portion, working on Style

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 // var userName = prompt('Hey there!  Welcome to my neato page.  What\'s your name?');
 // alert('Nice to meet you, ' + userName + ' . I\'m going to ask you some questions.  Hope you\'re ready for a game!');
-// console.log('The user said their name is ' + userName);
-var userName = 'test';
+//
 // var response1 = prompt('Do I have any pets?').toLowerCase();
 // var answer1 = 'no';
-
-// }
+//
 // if (response1 === answer1 || response1 === answer1[0]){
 //   alert('You are correct, ' + userName + '. No pets here!');
 // } else {
 //   alert('Nope!  Sorry, ' + userName + '.  No pets here!');
 // }
-var response2 = prompt('How many sisters do you think I have?');
-var answer2 = 6;
-
-if (response2 === 6) {
-    alert('Correct!  Kind of crazy, right ' + userName + '!');
-} else if (response2 < 6) {
-    alert('Nope!  I have more.  I know, it\'s hard to believe...');
-} else {
-    alert('Whoa, that would be a LOT!');
-}
+// var response2 = prompt('How many sisters do you think I have?');
+// var answer2 = 6;
+//
+// if (response2 === 6) {
+//   alert('Correct!  Kind of crazy, right ' + userName + '!');
+// } else if (response2 < 6) {
+//   alert('Nope!  I have more.  7.  I know, it\'s hard to believe...');
+// } else {
+//   alert('Whoa, that would be a LOT!');
+// }
+// var response3 = prompt('How many watches do I own?');
+// var answer3 = 7;
+// if (response3 === 7) {
+//   alert('Yup!  Totally right.  I kind of have a problem...');
+// } else if (response3 > 4 && response3 < 7) {
+//   alert('Not quite, but so close!  I actually have 7.');
+// } else if (response3 < 5) {
+//   alert('Not even close, ' + userName + '.  I actually have 7!');
+// } else if (response3 > 7) {
+//   alert('Whoa now. I don\'t have THAT many... Only 7...');
+// }
+//
+// alert('A little over halfway done!  2 more to go :D');
+//
+// var response4 = prompt('What kind of phone do I own, iPhone or Android?').toLowerCase();
+// var answer4 = 'android';
+//
+// if (response4 === answer4) {
+//   alert('Yup!  Android for life!');
+// } else {
+//   alert('Ew gross. Never.  You\'re not an Apple person, are you, ' + userName + '?');
+// }
+// var response5 = prompt('Coke or Pepsi?').toLowerCase();
+//
+// if (response5 === 'coke'){
+//   alert('Me too!  Pepsi is way too sweet to me.');
+// } else if (response5 === 'pepsi') {
+//   alert('I think Pepsi is WAAAY too sweet.  Just not my thing...');
+// } else {
+//   alert('Don\'t like either then?  I can respect that, ' + userName + '.');
+// }
